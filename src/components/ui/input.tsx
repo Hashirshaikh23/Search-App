5ff8ecadee2118f5602b4f45fd66aa9db8456cec
Hasir Shaikh <mohdhashshaikh@gmail.com>
2024-10-09 02:03:43 +0530

    Fix ESLint error in input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  // You can add any additional props here if needed
+  // You can add custom props here if needed
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
